ee87208114818a98d96c3cf5da89b607b7a1a654
kbannach <krzysztof92b@gmail.com>
2017-03-05 21:09:18 +0100

    finished deserializing od plain primitives

--- src/main/java/ug/jvm/deserializer/JsonDeserializer.java
+++ src/main/java/ug/jvm/deserializer/JsonDeserializer.java
@@ -4,11 +4,31 @@ import static ug.jvm.json.JsonSyntax.ARRAY_END;
 import static ug.jvm.json.JsonSyntax.ARRAY_START;
 import static ug.jvm.json.JsonSyntax.OBJECT_END;
 import static ug.jvm.json.JsonSyntax.OBJECT_START;
+import java.beans.IntrospectionException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 import ug.jvm.json.JsonSyntax;
 import ug.jvm.json.JsonSyntaxNotValidException;
+import ug.jvm.reflection.BeanFieldUtils;
 
 public class JsonDeserializer {
+
+   private static Map<String, Class< ? >> PRIMITIVE_TYPES_MAP = new HashMap<String, Class< ? >>() {
+
+                                                                 {
+                                                                    put("boolean", Boolean.class);
+                                                                    put("byte", Byte.class);
+                                                                    put("char", Character.class);
+                                                                    put("short", Short.class);
+                                                                    put("int", Integer.class);
+                                                                    put("long", Long.class);
+                                                                    put("float", Float.class);
+                                                                    put("double", Double.class);
+                                                                 }
+                                                              };
 
    public <T> T fromJson(String json, Class<T> type) {
       json = json.trim();
@@ -17,8 +37,55 @@ public class JsonDeserializer {
       } else if (wraps(json, OBJECT_START, OBJECT_END)) {
          // object
          Map<String, String> nameValueStringMap = fetchMapFromJson(json);
+         return deserialize(type, nameValueStringMap);
       }
       throw new JsonSyntaxNotValidException("First character is not array nor object.\n Json: " + json);
+   }
+
+   private <T> T deserialize(Class<T> type, Map<String, String> nameValueStringMap) {
+      try {
+         T instance = type.newInstance();
+         for (Field f : type.getDeclaredFields()) {
+            String newValueStr = nameValueStringMap.get(f.getName());
+            if (newValueStr == null) {
+               continue;
+            } else if (newValueStr.startsWith("[")) {
+               // TODO
+            } else if (newValueStr.startsWith("{")) {
+               // TODO
+            }
+            setSimpleNewValue(f, newValueStr, instance);
+         }
+         return instance;
+      } catch (InstantiationException | IllegalAccessException | IntrospectionException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+         throw new RuntimeException("Error during deserialization.", e);
+      }
+   }
+
+   /**
+    * Builds {@code f} field setter and invokes it with a {@code newValue} as a
+    * parameter. If {@code f} is a primitive type field, then its value is set
+    * by a boxed parameter.
+    */
+   private void setSimpleNewValue(Field f, String newValueStr, Object instance) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+         InstantiationException, NoSuchMethodException, SecurityException {
+      Method setter = BeanFieldUtils.buildSetter(f);
+      setter.invoke(instance, getObjectValue(f, newValueStr));
+   }
+
+   /**
+    * parses {@code newValueStr} using a field type's constructor
+    */
+   private Object getObjectValue(Field f, String newValueStr) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
+         SecurityException {
+      Class< ? > fieldType = f.getType();
+      if (fieldType.isPrimitive()) {
+         fieldType = PRIMITIVE_TYPES_MAP.get(f.getType().getName());
+      }
+      if (fieldType.equals(String.class)) {
+         newValueStr = ((String) newValueStr).substring(1, ((String) newValueStr).length() - 1);
+      }
+      return fieldType.getConstructor(String.class).newInstance(newValueStr);
    }
 
    /**
