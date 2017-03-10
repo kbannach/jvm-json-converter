2bf8cff82f95a491da91a1b46b7e2fcb510f35c1
kbannach <krzysztof92b@gmail.com>
2017-03-10 11:48:34 +0100

    Deserializing collections (in progress)

--- src/main/java/ug/jvm/deserializer/JsonDeserializer.java
+++ src/main/java/ug/jvm/deserializer/JsonDeserializer.java
@@ -8,6 +8,8 @@ import java.beans.IntrospectionException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import ug.jvm.json.JsonSyntax;
 import ug.jvm.json.JsonSyntaxNotValidException;
@@ -34,7 +36,7 @@ public class JsonDeserializer {
       }
 
       /**
-       * returns a Class object representation of a primitive of given name
+       * @return a Class object representation of a primitive of given name
        */
       public static final Class< ? > get(String name) {
          for (PRIMITIVE_TYPES_MAP v : values()) {
@@ -50,15 +52,30 @@ public class JsonDeserializer {
       json = json.trim();
       if (wraps(json, ARRAY_START, ARRAY_END)) {
          // array
+         // ignore
       } else if (wraps(json, OBJECT_START, OBJECT_END)) {
          // object
          Map<String, String> nameValueStringMap = fetchMapFromJson(json);
-         return deserialize(type, nameValueStringMap);
+         return deserializeObject(type, nameValueStringMap);
       }
-      throw new JsonSyntaxNotValidException("First character is not array nor object.\n Json: " + json);
+      throw new JsonSyntaxNotValidException("First character is not an object.\n Json: " + json);
    }
 
-   private <T> T deserialize(Class<T> type, Map<String, String> nameValueStringMap) {
+// TODO add comments inside this method
+   @SuppressWarnings("unchecked")
+   private <T> List<T> deserializeCollection(Class<T> genericType, List<String> arrayStrings) {
+      List<T> collection = new ArrayList<>(arrayStrings.size());
+      for (String item : arrayStrings) {
+         if (BeanFieldUtils.isJsonPrimitive(field)) {
+         }
+         // TODO
+//            collection.add(e);
+      }
+      // TODO
+      return collection;
+   }
+
+   private <T> T deserializeObject(Class<T> type, Map<String, String> nameValueStringMap) {
       try {
          T instance = type.newInstance();
          for (Field f : type.getDeclaredFields()) {
@@ -79,8 +96,13 @@ public class JsonDeserializer {
       }
    }
 
-   private void setCollectionValue(Field f, String newValueStr, Object instance) {
-      // TODO Auto-generated method stub
+   /**
+    * Builds {@code f} field setter and invokes it with an object parsed from
+    * {@code newValueStr} json as a parameter.
+    */
+   private void setCollectionValue(Field f, String newValueStr, Object instance) throws IntrospectionException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+      Method setter = BeanFieldUtils.buildSetter(f);
+      setter.invoke(instance, deserializeCollection(f.getGenericType().getClass(), fetchArrayFromJson(newValueStr)));
    }
 
    /**
@@ -108,10 +130,7 @@ public class JsonDeserializer {
     */
    private Object buildObjectValue(Field f, String newValueStr) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
          SecurityException {
-      Class< ? > fieldType = f.getType();
-      if (fieldType.isPrimitive()) {
-         fieldType = PRIMITIVE_TYPES_MAP.get(f.getType().getName());
-      }
+      Class< ? > fieldType = getObjectTypeFromPrimitive(f.getType());
       if (fieldType.equals(String.class)) {
          // remove quotes for strings
          newValueStr = ((String) newValueStr).substring(1, ((String) newValueStr).length() - 1);
@@ -119,12 +138,25 @@ public class JsonDeserializer {
       return fieldType.getConstructor(String.class).newInstance(newValueStr);
    }
 
+   private Class< ? > getObjectTypeFromPrimitive(Class< ? > fieldType) {
+      if (fieldType.isPrimitive()) {
+         fieldType = PRIMITIVE_TYPES_MAP.get(fieldType.getName());
+      }
+      return fieldType;
+   }
+
    /**
-    * @param json
+    * @return a list representing json array
+    */
+   private List<String> fetchArrayFromJson(String json) {
+      return JsonToStringMapConverter.convertArray(json);
+   }
+
+   /**
     * @return &lt;field name&gt; : &lt;string representing field value&gt; map
     */
    private Map<String, String> fetchMapFromJson(String json) {
-      return JsonToStringMapConverter.convert(json);
+      return JsonToStringMapConverter.convertMap(json);
    }
 
    private boolean wraps(String json, JsonSyntax startsWithElement, JsonSyntax endsWithElement) {
